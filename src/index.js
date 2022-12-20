4f6a6b32daaea23cfb3f877a239afa749d45873e
Marcus <marcus.nguyen.goldenowl@gmail.com>
2022-12-20 17:45:35 +0700

    Node-sass

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client';
 import App from 'core/App';
 import reportWebVitals from 'reportWebVitals';
 
-import 'assets/scss/global.css';
+import 'assets/scss/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
