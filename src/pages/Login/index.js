c4712800707f8602298db06aec755072051f0502
Marcus <marcus.nguyen.goldenowl@gmail.com>
2022-12-21 15:30:10 +0700

    Captcha SignIn

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,25 @@
+import React, { useRef } from 'react';
+
 import authentication from 'global/redux/requests/auth';
-import React from 'react';
+
+import ReCAPTCHA from 'react-google-recaptcha';
+import { toast } from 'react-toastify';
+import options from 'utils/constants/toast';
 
 const Login = () => {
+	const captchaRef = useRef(null);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		const {
-			email: { value: email },
-			password: { value: password },
-		} = e.target;
-
-		authentication.signIn(email, password);
+		const captchaToken = captchaRef.current.getValue();
+		if (captchaToken.length) {
+			const {
+				email: { value: email },
+				password: { value: password },
+			} = e.target;
+			authentication.signIn(email, password);
+			toast.success('LogIn !', options.default);
+		} else toast.error('Check captcha !', options.default);
 	};
 
 	const handleForgotPassword = () => {
@@ -21,6 +31,10 @@ const Login = () => {
 			<form className='form' onSubmit={handleSubmit}>
 				<input type='text' name='email' />
 				<input type='password' name='password' />
+				<ReCAPTCHA
+					ref={captchaRef}
+					sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+				/>
 				<button type='submit'>Login</button>
 			</form>
 			<button onClick={handleForgotPassword}>Reset Email</button>
