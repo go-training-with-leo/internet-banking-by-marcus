bde946f26d218f40fc43204e973172767cf454aa
Marcus <marcus.nguyen.goldenowl@gmail.com>
2023-01-05 14:31:43 +0700

    Change status in thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import 'services/wdyr';
-
 import 'react-hot-loader';
-import { hot } from 'react-hot-loader/root';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from 'core/App';
+import { hot } from 'react-hot-loader/root';
 import reportWebVitals from 'reportWebVitals';
 
-import 'normalize.css';
-import 'assets/scss/global.scss';
+import App from 'core/App';
+
+import 'services/wdyr';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const HotApp = hot(App);
