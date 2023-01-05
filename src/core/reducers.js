150c76c89f4f8dfdbf7b88f6e86e95fbaa0f4bbc
Marcus <marcus.nguyen.goldenowl@gmail.com>
2023-01-06 01:04:18 +0700

    Fix loading

--- src/core/reducers.js
+++ src/core/reducers.js
@@ -1,9 +1,17 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import auth from 'global/redux/auth/slice';
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  blacklist: ['isLoading'],
+};
+
 const reducers = combineReducers({
-  auth,
+  auth: persistReducer(authPersistConfig, auth),
 });
 
 export default reducers;
