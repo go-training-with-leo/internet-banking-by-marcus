bace4e4a4bf16ba23112a723e67dfb8c9e9e1d1f
Marcus <marcus.nguyen.goldenowl@gmail.com>
2023-01-11 19:53:13 +0700

    Complete Employee History

---
+++ src/views/History/Employee/tempData.js
@@ -0,0 +1,82 @@
+const tempData = [
+  {
+    id: 'CM1',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM2',
+    account: 'Justin Doe',
+    phone: '0183908118',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM3',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM4',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM5',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM6',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM7',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM8',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM9',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM10',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM11',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM12',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+  {
+    id: 'CM13',
+    account: 'Justin Doe',
+    phone: '0813908117',
+    email: 'email@gmail.com',
+  },
+];
+
+export default tempData;
