f61cb415f0fa85528fc6029eea0bce1586d942ad
Marcus <marcus.nguyen.goldenowl@gmail.com>
2023-01-16 09:39:52 +0700

    Build account view

--- src/views/Account/index.js
+++ src/views/Account/index.js
@@ -1,14 +1,29 @@
+import DefaultButton from 'components/Button/Default';
 import React from 'react';
 
-import DefaultButton from 'components/Button/Default';
-import { signOut } from 'global/redux/auth/request';
+import './style.scss';
 
 const Account = () => {
   return (
-    <div>
-      <DefaultButton danger onClick={() => signOut()}>
-        Log Out
-      </DefaultButton>
+    <div className='account-view'>
+      <div className='account-view-row'>
+        <span className='title'>Name:</span>
+        <span>Justin Doe</span>
+      </div>
+      <div className='account-view-row'>
+        <span className='title'>Phone:</span>
+        <span>+84381234567</span>
+      </div>
+      <div className='account-view-row'>
+        <span className='title'>Email:</span>
+        <span>email@gmail.com</span>
+      </div>
+      <div className='account-view-btn'>
+        <DefaultButton danger>Change password</DefaultButton>
+      </div>
+      <div className='account-view-btn'>
+        <DefaultButton danger>Log out</DefaultButton>
+      </div>
     </div>
   );
 };
