3386da61a1484b629c3e6a67828f7ae7a018ce97
Marcus <marcus.nguyen.goldenowl@gmail.com>
2023-01-16 08:07:45 +0700

    Create 3 tabs in history

--- src/views/History/Customer/index.js
+++ src/views/History/Customer/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+import './style.scss';
+
+const RECEIVE_TAB = 'RECEIVE_TAB';
+const TRANSFER_TAB = 'TRANSFER_TAB';
+const DEBT_REPAY_TAB = 'DEBT_REPAY_TAB';
 
 const CustomerHistory = () => {
-  return <div>Customer History</div>;
+  const [activeTab, setActiveTab] = useState(RECEIVE_TAB);
+
+  return (
+    <div className='customer-history-view'>
+      <div className='customer-history-view-tabs'>
+        <span
+          className={classNames({ active: activeTab === RECEIVE_TAB })}
+          onClick={() => setActiveTab(RECEIVE_TAB)}
+          role='tab'
+          tabIndex={0}
+        >
+          Receive
+        </span>
+        <span
+          className={classNames({ active: activeTab === TRANSFER_TAB })}
+          onClick={() => setActiveTab(TRANSFER_TAB)}
+          role='tab'
+          tabIndex={0}
+        >
+          Transfer
+        </span>
+        <span
+          className={classNames({ active: activeTab === DEBT_REPAY_TAB })}
+          onClick={() => setActiveTab(DEBT_REPAY_TAB)}
+          role='tab'
+          tabIndex={0}
+        >
+          Debt repaying
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default CustomerHistory;
