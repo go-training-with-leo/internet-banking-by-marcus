422fb7da968c8235726b829f3c54823b69714ae6
Marcus <marcus.nguyen.goldenowl@gmail.com>
2023-02-01 16:55:49 +0700

    Add saving card UI

--- src/views/Cards/NewSvCard/index.js
+++ src/views/Cards/NewSvCard/index.js
@@ -1,34 +1,68 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import Input from 'components/Input';
 import Modal from 'components/Modal';
 import DefaultButton from 'components/Button/Default';
+import { useSelector } from 'react-redux';
+import { selectCard } from 'core/selectors';
+import { divideSpaceIdCard } from 'utils/helpers';
+import validSavingCard from './validation';
 
 import './style.scss';
 
 const NewSvCard = ({ setToggle }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm({ resolver: yupResolver(validSavingCard) });
+
+  const { payingCard } = useSelector(selectCard);
+
+  const onCreate = (formData) => {
+    if (payingCard.balance - formData.balance < 0) {
+      setError('balance', {
+        type: 'custom',
+        message: 'Balance must at least 10 000 VND',
+      });
+      return;
+    }
+    console.warn('Log');
+  };
+
   return (
     <Modal title='New saving card' setToggle={setToggle} clickOutSide cancel>
-      <form className='new-card'>
+      <form className='new-card' onSubmit={handleSubmit(onCreate)}>
         <Input
+          disabled
           name='cardNumber'
           label='Card number'
-          placeholder='Card number'
+          placeholder={divideSpaceIdCard(payingCard?.cardNumber)}
         />
         <Input
+          register={register}
           name='timeDeposit'
           label='Time savings deposit'
           placeholder='Time savings deposit'
         />
         <Input
+          register={register}
           name='balance'
-          label='Balance'
+          error={errors?.balance && true}
+          label={
+            errors?.balance ? 'Balance must at least 10 000 VND' : 'Balance'
+          }
           placeholder='Enter your balance'
         />
         <div className='btn-group'>
-          <DefaultButton>Cancel</DefaultButton>
-          <DefaultButton danger>Create</DefaultButton>
+          <DefaultButton onClick={setToggle}>Cancel</DefaultButton>
+          <DefaultButton type='submit' danger>
+            Create
+          </DefaultButton>
         </div>
       </form>
     </Modal>
